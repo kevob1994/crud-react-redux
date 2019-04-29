dc449c195ccef17c8d228222a9742ce3cc937acb
Administrator FS <kevob1994@gmail.com>
2019-04-29 09:34:36 -0400

    new push

--- src/store.js
+++ src/store.js
@@ -1,9 +1,9 @@
-import { createStore, compose } from "redux";
-import rootReducer from "./reducers/root.reducer";
+import { createStore, compose } from 'redux';
+import rootReducer from './reducers/root.reducer';
 
 let devTool = null;
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === 'development') {
   devTool = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     : null;
